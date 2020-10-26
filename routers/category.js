ab57a57024c665d7b419aea079a27e5c63feaadc
thaibao98 <thaibao2990@gmail.com>
2020-10-26 14:21:14 +0700

    read category, delete-update category ...

--- routers/category.js
+++ routers/category.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const {
+  create,
+  categoryById,
+  update,
+  remove,
+  list,
+  read,
+} = require("../controllers/category");
 const { requestSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 router.post("/category/create/:userId", requestSignin, isAuth, isAdmin, create);
+router.get("/category/:categoryId", read);
 
+router.put(
+  "/category/:categoryId/:userId",
+  requestSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+router.delete(
+  "/category/:categoryId/:userId",
+  requestSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.get(
+  "/category/:categoryId/:userId",
+  requestSignin,
+  isAuth,
+  isAdmin,
+  list
+);
+
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 
 module.exports = router;
