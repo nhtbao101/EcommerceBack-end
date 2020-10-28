5c40e5ecc96f12fb63696ebdca9be5e2f0de4c13
thaibao98 <thaibao2990@gmail.com>
2020-10-29 01:08:37 +0700

    related product with id of category

--- controllers/product.js
+++ controllers/product.js
@@ -5,8 +5,9 @@ const errorHandler = require("../helper/errDBHandler");
 const Product = require("../models/product");
 const { resolveSoa } = require("dns");
 const product = require("../models/product");
+const category = require("../models/category");
 
-exports.create = async (req, res) => {
+exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
@@ -17,7 +18,15 @@ exports.create = async (req, res) => {
       });
     }
 
-    const { name, description, price, category, quantity, shipping } = fields;
+    const {
+      name,
+      description,
+      price,
+      sold,
+      category,
+      quantity,
+      shipping,
+    } = fields;
     if (
       !name ||
       !description ||
@@ -52,6 +61,54 @@ exports.create = async (req, res) => {
     });
   });
 };
+
+//asycn await
+// exports.create = async (req, res) => {
+//   try {
+//     let form = new formidable.IncomingForm();
+//     form.keepExtensions = true;
+//     const { fields, files } = await form.parse(req);
+//     // console.log(fields, "file====", file);
+//     if (!fields || !files)
+//       return res.status(400).json({
+//         error: "All fields are required ",
+//       });
+//     const { name, description, price, category, quantity, shipping } = fields;
+//     if (
+//       !name ||
+//       !description ||
+//       !price ||
+//       !category ||
+//       !quantity ||
+//       !shipping
+//     ) {
+//       return res.status(400).json({
+//         error: "All fields are required ",
+//       });
+//     }
+//     let product = new Product(fields);
+//     if (files.photo) {
+//       if (files.photo.size > 1000000) {
+//         return res.status(400).json({
+//           error: "Image should be less than 1Mb",
+//         });
+//       }
+//       product.photo.data = fs.readFileSync(files.photo.path);
+//       product.photo.contentType = files.photo.type;
+//     }
+//     const result = await product.save();
+//     if (!result) {
+//       return res.status(400).json({
+//         error: "Error ",
+//       });
+//     }
+//     return res.json(result);
+//   } catch (e) {
+//     return res.status(400).json({
+//       error: "Error ",
+//     });
+//   }
+// };
 
 exports.producById = async (req, res, next, id) => {
   try {
@@ -154,6 +211,23 @@ exports.list = async (req, res) => {
   } catch (err) {
     return res.status(400).json({
       err: err,
+    });
+  }
+};
+
+exports.relatedProduct = async (req, res) => {
+  try {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+    let products = await Product.find({
+      _id: { $ne: req.product },
+      category: req.product.category,
+    })
+      .limit(limit)
+      .populate("category", "_id name");
+    res.json(products);
+  } catch (error) {
+    return res.status(400).json({
+      error: error,
     });
   }
 };
--- routers/product.js
+++ routers/product.js
@@ -8,6 +8,7 @@ const {
   remove,
   update,
   list,
+  relatedProduct,
 } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 const { requestSignin, isAdmin, isAuth } = require("../controllers/auth");
@@ -32,6 +33,7 @@ router.put(
 );
 
 router.get("/products", list);
+router.get("/product/related/:productId", relatedProduct);
 
 router.param("userId", userById);
 router.param("productId", producById);
